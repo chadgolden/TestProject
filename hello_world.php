2ad1a9b660cf4291d3f722530c572164141708ab
chadgolden <chad@chadgolden.com>
2015-02-09 21:30:39 -0500

    Testing

--- hello_world.php
+++ hello_world.php
@@ -5,7 +5,22 @@
  * Date: 2/9/2015
  * Time: 8:29 PM
  */
+?>
 
-echo 'Hello World!';
+<!DOCTYPE html>
 
-echo 'This is a test.';
+<html>
+
+    <head>
+        <title>
+            Some Title
+        </title>
+    </head>
+
+    <body>
+        <h1>
+            <?php echo 'Hello World!'; ?>
+        </h1>
+    </body>
+
+</html>
